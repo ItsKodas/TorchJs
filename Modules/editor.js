90a96eed72fd706198ae1a90e020ab4332bd9ad8
Jessica <koda@horizons.gg>
2022-06-01 10:31:08 +1000

    Hotfix

--- Modules/editor.js
+++ Modules/editor.js
@@ -40,40 +40,40 @@ async function UpdateTorchCFG(obj, r) {
 
 
 async function GetSEDConfig() {
-    return await ParseXML(`${process.env.dir}/${process.env.instance}/SpaceEngineers-Dedicated.cfg`).catch(err => console.log(err))
+    return await ParseXML(`${process.env.instances || process.env.dir}/${process.env.instance}/SpaceEngineers-Dedicated.cfg`).catch(err => console.log(err))
 }
 
 async function UpdateSEDConfig(obj, r) {
-    fs.writeFileSync(`${process.env.dir}/${process.env.instance}/SpaceEngineers-Dedicated.cfg`, await BuildXML(obj, r))
+    fs.writeFileSync(`${process.env.instances || process.env.dir}/${process.env.instance}/SpaceEngineers-Dedicated.cfg`, await BuildXML(obj, r))
 }
 
 
 
 async function GetSandboxSBC() {
-    return await ParseXML(`${process.env.dir}/${process.env.instance}/Saves/${process.env.world}/Sandbox.sbc`).catch(() => Rollback.WorldFile('Sandbox.sbc'))
+    return await ParseXML(`${process.env.instances || process.env.dir}/${process.env.instance}/Saves/${process.env.world}/Sandbox.sbc`).catch(() => Rollback.WorldFile('Sandbox.sbc'))
 }
 
 async function GetSandboxSBS() {
-    return await ParseXML(`${process.env.dir}/${process.env.instance}/Saves/${process.env.world}/SANDBOX_0_0_0_.sbs`).catch(() => Rollback.WorldFile('SANDBOX_0_0_0_.sbs'))
+    return await ParseXML(`${process.env.instances || process.env.dir}/${process.env.instance}/Saves/${process.env.world}/SANDBOX_0_0_0_.sbs`).catch(() => Rollback.WorldFile('SANDBOX_0_0_0_.sbs'))
 }
 
 async function GetSandboxConfig() {
-    return await ParseXML(`${process.env.dir}/${process.env.instance}/Saves/${process.env.world}/Sandbox_config.sbc`).catch(() => Rollback.WorldFile('Sandbox_config.sbc'))
+    return await ParseXML(`${process.env.instances || process.env.dir}/${process.env.instance}/Saves/${process.env.world}/Sandbox_config.sbc`).catch(() => Rollback.WorldFile('Sandbox_config.sbc'))
 }
 
 
 
 async function UpdateSandboxSBC(obj, r) {
-    fs.writeFileSync(`${process.env.dir}/${process.env.instance}/Saves/${process.env.world}/Sandbox.sbc`, await BuildXML(obj, r))
+    fs.writeFileSync(`${process.env.instances || process.env.dir}/${process.env.instance}/Saves/${process.env.world}/Sandbox.sbc`, await BuildXML(obj, r))
 }
 
 async function UpdateSandboxSBS(obj, r) {
-    fs.writeFileSync(`${process.env.dir}/${process.env.instance}/Saves/${process.env.world}/SANDBOX_0_0_0_.sbs`, await BuildXML(obj, r))
-    if (fs.existsSync(`${process.env.dir}/${process.env.instance}/Saves/${process.env.world}/SANDBOX_0_0_0_.sbsB5`)) fs.unlinkSync(`${process.env.dir}/${process.env.instance}/Saves/${process.env.world}/SANDBOX_0_0_0_.sbsB5`)
+    fs.writeFileSync(`${process.env.instances || process.env.dir}/${process.env.instance}/Saves/${process.env.world}/SANDBOX_0_0_0_.sbs`, await BuildXML(obj, r))
+    if (fs.existsSync(`${process.env.instances || process.env.dir}/${process.env.instance}/Saves/${process.env.world}/SANDBOX_0_0_0_.sbsB5`)) fs.unlinkSync(`${process.env.instances || process.env.dir}/${process.env.instance}/Saves/${process.env.world}/SANDBOX_0_0_0_.sbsB5`)
 }
 
 async function UpdateSandboxConfig(obj, r) {
-    fs.writeFileSync(`${process.env.dir}/${process.env.instance}/Saves/${process.env.world}/Sandbox_config.sbc`, await BuildXML(obj, r))
+    fs.writeFileSync(`${process.env.instances || process.env.dir}/${process.env.instance}/Saves/${process.env.world}/Sandbox_config.sbc`, await BuildXML(obj, r))
 }
 
 
--- Modules/rollback.js
+++ Modules/rollback.js
@@ -4,13 +4,13 @@ const fs = require('fs')
 
 async function WorldFile(file) {
 
-    if (!fs.existsSync(`${process.env.dir}\\${process.env.instance}\\Saves\\${process.env.world}\\Backup`)) return console.log(`A Critical World File is Missing and no backups are available: ${file}`), process.exit(1)
+    if (!fs.existsSync(`${process.env.instances || process.env.dir}\\${process.env.instance}\\Saves\\${process.env.world}\\Backup`)) return console.log(`A Critical World File is Missing and no backups are available: ${file}`), process.exit(1)
 
-    const Backups = fs.readdirSync(`${process.env.dir}\\${process.env.instance}\\Saves\\${process.env.world}\\Backup`)
+    const Backups = fs.readdirSync(`${process.env.instances || process.env.dir}\\${process.env.instance}\\Saves\\${process.env.world}\\Backup`)
 
-    if (!fs.existsSync(`${process.env.dir}\\${process.env.instance}\\Saves\\${process.env.world}\\Backup\\${Backups[Backups.length - 1]}\\${file}`)) return console.log(`A Critical World File is Missing and no backups are available: ${file}`), process.exit(1)
+    if (!fs.existsSync(`${process.env.instances || process.env.dir}\\${process.env.instance}\\Saves\\${process.env.world}\\Backup\\${Backups[Backups.length - 1]}\\${file}`)) return console.log(`A Critical World File is Missing and no backups are available: ${file}`), process.exit(1)
 
-    return await require('./editor.js').ParseXML(`${process.env.dir}\\${process.env.instance}\\Saves\\${process.env.world}\\Backup\\${Backups[Backups.length - 1]}\\${file}`).catch(err => { throw new error(err) })
+    return await require('./editor.js').ParseXML(`${process.env.instances || process.env.dir}\\${process.env.instance}\\Saves\\${process.env.world}\\Backup\\${Backups[Backups.length - 1]}\\${file}`).catch(err => { throw new error(err) })
 
 }
 

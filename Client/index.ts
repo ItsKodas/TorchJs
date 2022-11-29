33ce6d462799e24435b0109ff272e32b306b3357
Koda <koda@horizons.gg>
2022-11-29 19:40:34 +1000

    Prepped Electron App

--- Client/index.ts
+++ Client/index.ts
@@ -1,59 +1,19 @@
 //? Dependencies
 
-import Config from '@lib/config'
-
-import Query from '@api/standard'
-
-import Client from '@app/client'
+import { app, BrowserWindow } from 'electron'
 
 
 
-//! Initialize Client with Server
+//? Application Window
 
-(async () => {
-
-    for (const instance of Config.instances) {
-
-        if (!instance.enabled) {
-            console.warn(`Skipping '${instance.id}' as it is disabled.`)
-            continue
-        }
-
-
-        const client = new Client(instance.id)
-
-        await ConnectInstance(client)
-            .then(res => console.info(res))
-            .catch(err => console.error(err))
-
-        if (!client.isConnected) return
-
-
-        await client.start()
-            .then(res => console.info(res))
-            .catch(err => console.error(err))
-    }
-
-})();
-
-
-
-async function ConnectInstance(client: Client) {
-    return new Promise((resolve, reject) => {
-
-        let attempts = 0
-
-        function Connect() {
-            client.connect()
-                .then(resolve)
-                .catch(err => {
-                    if (attempts >= 10) return reject(`Failed to Connect '${client.id}' to the Server after ${attempts} attempts.`)
-                    console.error(err, `| Attempt: ${attempts++}`)
-                    setTimeout(Connect, 10000)
-                })
-        }
-
-        Connect()
-
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
     })
+
+    win.loadURL('https://new.horizons.gg')
 }
+
+
+app.on('ready', () => createWindow())
